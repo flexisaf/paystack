656d61737f9c524c83a5746bd4197a77a68441f2
pitaside <fr33wayz@gmail.com>
2016-12-16 14:00:58 +0100

    add .travis file for custom build

--- travis/build.sh
+++ travis/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [[ "$ENV" == 'master' ]];
-    echo "Herer"
-#  ./gradlew clean check test publish
-then
-    echo "Hey"
-#  ./gradlew clean check test
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  ./gradlew clean check test publish
+else
+  ./gradlew clean check test
 fi
